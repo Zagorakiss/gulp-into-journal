7dfe90ca9f63e5f8793b1abac9acb1e928827745
Zagora <zagorakom@gmail.com>
2016-08-22 14:45:21 +0300

    Mobile Version + Mobile Navigation
    21.08.2016

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,7 +12,28 @@ var Page = (function() {
 		config.$bookBlock.bookblock( {
 			speed : 1000,
 			shadowSides : 0.8,
-			shadowFlip : 0.4
+			shadowFlip : 0.4,
+			onEndFlip: function(old, page, isLimit ) {
+				page = page + 1;
+
+				var pageUrl = '/nastroenie-more';
+				var arPageNumbers = {
+					1: '/mechta-detstva/',
+					2: '/mechta-detstva/#2',
+					3: '/morskaya-tatoo-istoriya/',
+					4: '/morskaya-tatoo-istoriya/#2',
+					5: '/gorod/',
+					6: '/gorod/#2',
+					7: '/gorod/#3',
+					8: '/telnashka/',
+					9: '/kapitan-sudna/',
+					10: '/v-samostoyatelnom-plavanii/',
+					11: '/v-samostoyatelnom-plavanii/#2'
+				};
+				
+				history.replaceState('', '', pageUrl + arPageNumbers[page]); 
+				return false;
+			}
 		} );
 		initEvents();
 	},
@@ -84,12 +105,44 @@ Page.init();
 
 
 
+
+// Page Jumping /////////////////////////////////////////////////////////////////////////////////
+// $(document).ready(function(){
+// 	if (window.location.href.indexOf("#1") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 1 );}
+// 	if (window.location.href.indexOf("#2") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 3 );}
+// 	if (window.location.href.indexOf("#3") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 5 );}
+// 	if (window.location.href.indexOf("#4") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 8 );}
+// 	if (window.location.href.indexOf("#5") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 9 );}
+// 	if (window.location.href.indexOf("#6") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 10 );}
+
+// });
+// Page Jumping END //////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+// Sidebar ///////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function(){
-	if (window.location.href.indexOf("#1") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 1 );}
-	if (window.location.href.indexOf("#2") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 3 );}
-	if (window.location.href.indexOf("#3") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 5 );}
-	if (window.location.href.indexOf("#4") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 8 );}
-	if (window.location.href.indexOf("#5") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 9 );}
-	if (window.location.href.indexOf("#6") !== -1) {$( '#bb-bookblock' ).bookblock( 'jump', 10 );}
+	var toggleBtnSidebar = document.querySelector('.sidebar__toggle');
+	var sidebar = document.querySelector('.sidebar');
+	var linkSidebar = $('.sidebar__link');
+	var journalContainer = $('.container_mobile-journal');
+
+	toggleBtnSidebar.addEventListener('click', function() {
+	  toggleBtnSidebar.classList.toggle('is-closed');
+	  sidebar.classList.toggle('is-closed');
+	});
+
+	linkSidebar.on('click', function() {
+		var pageToShow = $(this).attr('data-link');
+		journalContainer.attr('data-show', pageToShow);
+		journalContainer.animate({"scrollTop":0},"slow");
+		toggleBtnSidebar.classList.toggle('is-closed');
+		sidebar.classList.toggle('is-closed');
+	});
 
 });
+// Sidebar END ///////////////////////////////////////////////////////////////////////////////////
